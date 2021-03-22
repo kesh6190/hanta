b277118c6a9ec6a7ca76a35f27913ebbb8d9c01b
kesh6190 <skchundunsing@gmail.com>
2021-03-22 14:38:08 +0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+	  <title>Homepage</title>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	
+	<body>
+		<h1>This is the homepage</h1>
+	<body>
+</html>
---
+++ stats.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+	  <title>Homepage</title>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	
+	<body>
+		<h1>This is Stats page</h1>
+	<body>
+</html>
